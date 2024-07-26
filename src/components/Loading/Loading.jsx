ae52aa1777cf9369a9e927aaa374429688e5ddd7
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-07-26 16:35:02 +0530

    Pagination and Loader Styling done

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+function Loading() {
+  return (
+    <div className="loader-container">
+      <h1 className="loader-heading">LOADING...</h1>
+    </div>
+  );
+}
+
+export default Loading;
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -2,19 +2,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
+import Loading from "../Loading/Loading";
 
 function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
+  const [pokedexUrl, setPokedexUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
 
   async function downloadPokemons() {
-    const response = await axios.get(POKEDEX_URL); // this downloads list of 20 pokemons.
+    setIsLoading(true);
+    const response = await axios.get(pokedexUrl); // this downloads list of 20 pokemons.
 
     const pokemonResults = response.data.results; // we get the array of pokemons from result
 
     console.log(response.data);
+
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
 
     // iterating over the array of pokemons and using their url, to create an array of promises
     // that will download those 20 pokemons
@@ -40,27 +50,39 @@ function PokemonList() {
     });
     console.log(pokeListResult);
     setPokemonList(pokeListResult);
-    setLoading(false);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     downloadPokemons();
-  }, []);
+  }, [pokedexUrl]);
 
   return (
     <div className="pokemon-list-wrapper">
       <div className="pokemon-wrapper">
-        {isLoading
-          ? "Loading..."
-          : pokemonList.map((p) => (
-              <Pokemon name={p.name} image={p.image} key={p.id} />
-            ))}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          pokemonList.map((p) => (
+            <Pokemon name={p.name} image={p.image} key={p.id} />
+          ))
+        )}
       </div>
 
       {/* Pagination Controls   */}
       <div className="controls">
-        <button>Prev</button>
-        <button>Next</button>
+        <button
+          disabled={prevUrl === null}
+          onClick={() => setPokedexUrl(prevUrl)}
+        >
+          Prev
+        </button>
+        <button
+          disabled={nextUrl === null}
+          onClick={() => setPokedexUrl(nextUrl)}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
